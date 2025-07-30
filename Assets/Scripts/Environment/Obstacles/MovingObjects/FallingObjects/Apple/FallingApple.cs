dd7e61700afcf1c86093333bd5c0ff62c07c6e09
vzk <154731072+vzkstrx@users.noreply.github.com>
2025-07-30 17:13:07 +0300

    Added falling apples

---
+++ Assets/Scripts/Environment/Obstacles/MovingObjects/FallingObjects/Apple/FallingApple.cs
@@ -0,0 +1,47 @@
+using System.Threading;
+using Player;
+using UnityEngine;
+
+namespace Environment.Obstacles.MovingObjects.FallingObjects.Apple
+{
+    public class FallingApple : MovingObject
+    {
+        [SerializeField] private float _slowdownFactor = 0.5f;
+        [SerializeField] private float _effectDuration = 5;
+        
+        private static CancellationTokenSource _cancellationTokenSource;
+
+        protected override async void OnPlayerEnter(PlayerController player)
+        {
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+            
+            Destroy(gameObject);
+            ApplyTimeEffect();
+            
+            try
+            {
+                await Awaitable.WaitForSecondsAsync(_effectDuration, _cancellationTokenSource.Token);
+            }
+            catch
+            {
+                return;
+            }
+            
+            if (!_cancellationTokenSource.IsCancellationRequested) RestoreTime();
+        }
+
+        private void ApplyTimeEffect()
+        {
+            Time.timeScale = _slowdownFactor;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+
+        private static void RestoreTime()
+        {
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+            Debug.Log(Time.timeScale);
+        }
+    }
+}
